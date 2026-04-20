c2c7e3ce1aa9d89a8b4272f081ea2df29d820158
Antoine BOULINGUEZ <aboulinguez@trace-software.com>
2026-04-20 11:28:46 +0200

    delete all com

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,37 +1,37 @@
-﻿// Programme principal
-Console.WriteLine("Entrez le premier nombre : ");  // Affiche un message
-double nombre1 = double.Parse(Console.ReadLine()); // Lit et convertit en nombre
+﻿
+Console.WriteLine("Entrez le premier nombre : ");  
+double nombre1 = double.Parse(Console.ReadLine()); 
 
-Console.WriteLine("Entrez le deuxième nombre : "); // Affiche un message
-double nombre2 = double.Parse(Console.ReadLine()); // Lit et convertit en nombre
+Console.WriteLine("Entrez le deuxième nombre : "); 
+double nombre2 = double.Parse(Console.ReadLine()); 
 
-Console.WriteLine("Opération (+, -, *, /) : ");    // Affiche un message
-string operation = Console.ReadLine();              // Lit l'opération (pas besoin de convertir)
+Console.WriteLine("Opération (+, -, *, /) : ");   
+string operation = Console.ReadLine();             
 
-double resultat = 0; // On crée une variable pour stocker le résultat
+double resultat = 0; 
 
-switch (operation) // On vérifie quelle opération l'utilisateur a choisie
+switch (operation) 
 {
     case "+":
-        resultat = nombre1 + nombre2; // Addition
-        break; // On sort du switch
+        resultat = nombre1 + nombre2; //hello
+        break; 
     case "-":
-        resultat = nombre1 - nombre2; // Soustraction
+        resultat = nombre1 - nombre2; 
         break;
     case "*":
-        resultat = nombre1 * nombre2; // Multiplication
+        resultat = nombre1 * nombre2; 
         break;
     case "/":
-        if (nombre2 == 0) // On vérifie qu'on ne divise pas par 0
+        if (nombre2 == 0) 
         {
             Console.WriteLine("Erreur : division par zéro impossible !");
-            return; // On arrête le programme
+            return; 
         }
-        resultat = nombre1 / nombre2; // Division
+        resultat = nombre1 / nombre2; 
         break;
     default:
-        Console.WriteLine("Opération inconnue !"); // Si l'utilisateur tape autre chose
+        Console.WriteLine("Opération inconnue !"); 
         return;
 }
 
-Console.WriteLine("Résultat : " + resultat); // Affiche le résultat
+Console.WriteLine("Résultat : " + resultat); 
